567a3cc9179b8c4ccd7769ce439f580927022294
Mykyta Borysevych <mykyta.mail@gmail.com>
2021-04-27 17:47:09 +0200

    Code review fixes

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,8 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 import Vue from 'vue';
 import VueRouter from 'vue-router'
 import githubUsernameRegex from 'github-username-regex';
-// start the Stimulus application
+
 import './bootstrap';
 
 const app = new Vue({
@@ -40,8 +32,6 @@ const app = new Vue({
             }
             e.stopPropagation();
             e.preventDefault();
-
-
         }
     }
 });
--- src/DTO/Helper/BasicRequestDTO.php
+++ src/DTO/Helper/BasicRequestDTO.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace App\DTO\Helper;
-
 
 trait BasicRequestDTO
 {
